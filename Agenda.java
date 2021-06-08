afcc8c50523cd4d9e8aaf267eae671e61da80cab
danmora9854 <dan.mora9854@gmail.com>
2021-06-07 22:44:28 -0600

    Update Agenda.java

--- Agenda.java
+++ Agenda.java
@@ -1,1 +1,11 @@
-
+import java.util.ArrayList;
+import java.io.File;
+public class Agenda{
+  Arraylist<Tarea> agenda;
+  public Agenda(){
+  agenda=new Arraylist<Tarea>();
+  }
+  public static void main(String a[]){
+  //Controlador//
+  }
+}
