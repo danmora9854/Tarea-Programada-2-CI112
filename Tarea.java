0aab99cb8fcb558234f24cf8f5c38bff81c865b0
danmora9854 <dan.mora9854@gmail.com>
2021-06-07 22:40:35 -0600

    Update Tarea.java

--- Tarea.java
+++ Tarea.java
@@ -1,9 +1,33 @@
 import java.util.ArrayList;
 public class Tarea{
-String nombre;
-String id;
+ String nombre;
+ String id;
+ String estado;
+ ArrayList<String> recursos;
 public Tarea(String n, String i){
-nombre=n;
+ nombre=n;
  id=i;
+ estado="Pendiente";
+ recursos=new ArrayList<String>();
 }
+ public void cambieEstado(int n){
+  switch(n){
+   case n=1:
+   estado="Pendiente";
+   break;
+   case n=2;
+   estado="Haciendo";
+    break;
+   case n=3;
+   estado="Finalizado";
+    break;
+  }
+  public void agregaRecurso(String p){
+   recursos.add(p);
+  }
+  public void muestraRecursos(){
+   System.out.println(recursos);
+  }
+  
+ }
 }
