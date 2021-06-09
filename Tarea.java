4c8a4cb6966ede6a9470d9b2bd433cbb8ef965ea
JJBV-UCR <81998140+JJBV-UCR@users.noreply.github.com>
2021-06-09 00:14:03 -0600

    Update Tarea.java

--- Tarea.java
+++ Tarea.java
@@ -1,33 +1,74 @@
+
+/**
+ * Clase para manejar la informacion de una tarea
+ *
+ * @author (Juan José Bermúdez Vargas y Daniel Mora Mora )
+ * @version (08.06.2021)
+ */
+
 import java.util.ArrayList;
-public class Tarea{
- String nombre;
- String id;
- String estado;
- ArrayList<String> recursos;
-public Tarea(String n, String i){
- nombre=n;
- id=i;
- estado="Pendiente";
- recursos=new ArrayList<String>();
-}
- public void cambieEstado(int n){
-  switch(n){
-   case n=1:
-   estado="Pendiente";
-   break;
-   case n=2;
-   estado="Haciendo";
-    break;
-   case n=3;
-   estado="Finalizado";
-    break;
-  }
-  public void agregaRecurso(String p){
-   recursos.add(p);
-  }
-  public void muestraRecursos(){
-   System.out.println(recursos);
-  }
-  
- }
+import javax.swing.JOptionPane;
+public class Tarea
+{
+    public ArrayList <String> colaboradores;
+    public String [] infoAdmin;
+    public ArrayList <Nota> notas;
+    public int id;
+    
+    public Tarea ()
+    {
+        colaboradores = new ArrayList <String> ();
+        infoAdmin = new String [4];
+        notas = new ArrayList <Nota> ();
+        id = 0;
+    }
+    
+    public void agregueInfo ()
+    {
+        //Un menu donde elige qué de infoAdmin llenar
+        String [] ops = {"Fecha de inicio","Fecha de fin","Estimación","Grado de avance"};
+        
+        //Luego pregunta si quiere definir colaboradores para la tarea.
+        boolean hayColab = true;
+        if (hayColab)
+        {
+            //Aca pregunta por el tiempo que se le dará al grupo para completar la tarea y lo guarda en el primer espacio del ArrayList colaboradores
+            
+            //Aca pregunta por el número de colaboradores de la tarea, digamos N, y los lee junto al tiempo semanal que se les da
+            int N = 0;
+            boolean invalid = true;
+            while(invalid) {
+                try {
+                    String s = (String) (JOptionPane.showInputDialog(null,"Ingrese el número de colaboradores"));
+                    N = Integer.parseInt(s);
+                    invalid = false;
+                    if (N <= 0) {
+                        JOptionPane.showMessageDialog(null,"¡El número debe ser un entero positivo!");
+                        invalid = true;
+                    }
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null,"¡Ingrese un número válido!");
+                }
+            }
+            
+            for (int i = 0; i<N; i++)
+            {
+                //Pregunta por nombre del colaborador i y lo va guardando en el ArrayList colaboradores
+            }
+        }
+        
+        //Por último pregunta si quiere agregar alguna nota. Las notas son listas simples de elementos. Como en el ejemplo que sale en el word
+        //que mencionan una lista de recursos de la tarea.
+        boolean hayNotas = true;
+        while (hayNotas)
+        {
+            //pregunta con JOptionPane por el titulo de la nota.
+            String titulo = null;
+            Nota newNote = new Nota (titulo);
+            newNote.guardeElementos();
+            notas.add(newNote);
+            
+            //pregunta si quiere ingresar alguna otra nota. Si no, hace hayNotas = false
+        }
+    }
 }
