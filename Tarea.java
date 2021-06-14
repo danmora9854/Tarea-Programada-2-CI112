d1972d091b2743d1cdf41560c26a9c6ebe1b5011
JJBV-UCR <81998140+JJBV-UCR@users.noreply.github.com>
2021-06-13 21:35:31 -0600

    Update Tarea.java

--- Tarea.java
+++ Tarea.java
@@ -11,51 +11,44 @@ import javax.swing.JOptionPane;
 public class Tarea
 {
     public ArrayList <String> colaboradores;
-    public String [] infoAdmin;
+    public String [] atributos;//Aquí va estado de la tarea, complejidad y prioridad
     public ArrayList <Nota> notas;
     public int id;
     
     public Tarea ()
     {
         colaboradores = new ArrayList <String> ();
-        infoAdmin = new String [4];
+        atributos = new String [3];
         notas = new ArrayList <Nota> ();
+        notas.add(new Nota ("Información Administrativa"));
+        Nota infoAdmin = notas.get(0);
+        infoAdmin.recurso.add("Grado de avance");
+        infoAdmin.recurso.add("Colaborador");
+        infoAdmin.recurso.add("Tiempo asignado");
+        infoAdmin.recurso.add("Prioridad");
+        infoAdmin.recurso.add("Complejidad");
+        infoAdmin.recurso.add("Fecha de inicio");
+        infoAdmin.recurso.add("Fecha de fin");
+        infoAdmin.recurso.add("Estimación");
+        for (int i = 0; i<8; i++)
+        {
+            infoAdmin.cantidades.add("");
+        }
         id = 0;
     }
     
     public void agregueInfo ()
     {
         //Un menu donde elige qué de infoAdmin llenar
-        String [] ops = {"Fecha de inicio","Fecha de fin","Estimación","Grado de avance"};
-        
-        //Luego pregunta si quiere definir colaboradores para la tarea.
-        boolean hayColab = true;
-        if (hayColab)
+        String [] ops = {"Grado de avance","Colaborador","Tiempo Asignado","Prioridad","Complejidad","Fecha de inicio","Fecha de fin","Estimación"};
+        String ans = (String)(JOptionPane.showInputDialog(null,"Cuál entrada desea rellenar?","Por favor escoja una opción",JOptionPane.QUESTION_MESSAGE, null, ops, ops[0]));
+        int index = -1;
+        for (int i = 0; i<ops.length; i++)
         {
-            //Aca pregunta por el tiempo que se le dará al grupo para completar la tarea y lo guarda en el primer espacio del ArrayList colaboradores
-            
-            //Aca pregunta por el número de colaboradores de la tarea, digamos N, y los lee junto al tiempo semanal que se les da
-            int N = 0;
-            boolean invalid = true;
-            while(invalid) {
-                try {
-                    String s = (String) (JOptionPane.showInputDialog(null,"Ingrese el número de colaboradores"));
-                    N = Integer.parseInt(s);
-                    invalid = false;
-                    if (N <= 0) {
-                        JOptionPane.showMessageDialog(null,"¡El número debe ser un entero positivo!");
-                        invalid = true;
-                    }
-                } catch (NumberFormatException e) {
-                    JOptionPane.showMessageDialog(null,"¡Ingrese un número válido!");
-                }
-            }
-            
-            for (int i = 0; i<N; i++)
-            {
-                //Pregunta por nombre del colaborador i y lo va guardando en el ArrayList colaboradores
-            }
+            if (ops[i].equals(ans)) {index = i;}
         }
+        String data = (String)(JOptionPane.showInputDialog(null,"Ingrese data de la entrada"));
+        notas.get(0).cantidades.set(index,data);
         
         //Por último pregunta si quiere agregar alguna nota. Las notas son listas simples de elementos. Como en el ejemplo que sale en el word
         //que mencionan una lista de recursos de la tarea.
@@ -63,7 +56,7 @@ public class Tarea
         while (hayNotas)
         {
             //pregunta con JOptionPane por el titulo de la nota.
-            String titulo = null;
+            String titulo = (String) ;
             Nota newNote = new Nota (titulo);
             newNote.guardeElementos();
             notas.add(newNote);
