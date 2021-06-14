3bec05298c1d128b5d84fc82194b9b5c5a772dc7
JJBV-UCR <81998140+JJBV-UCR@users.noreply.github.com>
2021-06-14 02:05:27 -0600

    Update Nota.java

--- Nota.java
+++ Nota.java
@@ -37,23 +37,37 @@ public class Nota
             hayElementos = (ans.equals("Sí"))?true:false;
         }
     }
+    
     public String muestreElementos(){
         String display="Los recursos de esta tarea son: \n";
         for(int i=0;i<recurso.size();i++){display+=recurso.get(i)+" con "+cantidades.get(i)+" cantidades.\n";}
         return display;
     }
-    public void cambieElementos(int i){
+    
+    public void cambieElementos()
+    {
+        String[] elemNames = new String[recurso.size()];
+        for(int j=0;j<recurso.size();j++){
+            elemNames[j]=recurso.get(j);
+        }
+        String ans1 = (String)(JOptionPane.showInputDialog(null,"¿Cuál elemento desea modificar?","Por favor escoja una opción",JOptionPane.QUESTION_MESSAGE, null, elemNames, elemNames[0]));
+        int ind=-1;
+        for (int i = 0; i<elemNames.length; i++)
+        {
+            if (elemNames[i].equals(ans1)) {ind = i;}
+        }
+        
         boolean cambioElementos = true;
         while (cambioElementos)
         {
-            String elemento = (String)(JOptionPane.showInputDialog(null,"Ingrese tipo de elemento."));
-            String cantidad = (String)(JOptionPane.showInputDialog(null,"Ingrese cantidad de elemento."));
-            recurso.set(i,elemento);
-            cantidades.set(i,cantidad);
+            String elemento = (String)(JOptionPane.showInputDialog(null,"Ingrese nuevo nombre del elemento."));
+            String cantidad = (String)(JOptionPane.showInputDialog(null,"Ingrese nueva cantidad del elemento."));
+            recurso.set(ind,elemento);
+            cantidades.set(ind,cantidad);
             
             String [] ops = {"Sí","No"};
-            String ans = (String)(JOptionPane.showInputDialog(null,"Desea cambiar otro elemento?","Por favor escoja una opción",JOptionPane.QUESTION_MESSAGE, null, ops, ops[0]));
-            cambioElementos = (ans.equals("Sí"))?true:false;
+            String ans2 = (String)(JOptionPane.showInputDialog(null,"Desea cambiar otro elemento?","Por favor escoja una opción",JOptionPane.QUESTION_MESSAGE, null, ops, ops[0]));
+            cambioElementos = (ans2.equals("Sí"))?true:false;
         }
     }
 }
