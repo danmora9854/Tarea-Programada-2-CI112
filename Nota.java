a8d2448b4bfb4711b2e87ba3a7deafb67a807187
JJBV-UCR <81998140+JJBV-UCR@users.noreply.github.com>
2021-06-13 20:56:44 -0600

    Update Nota.java

--- Nota.java
+++ Nota.java
@@ -1,6 +1,7 @@
 
+
 /**
- * Clase que maneja las notas. Estas son listas simples de elementos.
+ * Clase que maneja las notas de recursos.
  *
  * @author (Juan José Bermúdez Vargas y Daniel Mora Mora)
  * @version (08.06.2021)
@@ -11,12 +12,14 @@ import javax.swing.JOptionPane;
 public class Nota
 {
     String titulo;
-    ArrayList <String> lista;
+    ArrayList <String> recurso;
+    ArrayList <Integer> cantidades;
     
     public Nota (String titulo)
     {
         this.titulo = titulo;
-        lista = new ArrayList <String> ();
+        recurso = new ArrayList <String> ();
+        cantidades = new ArrayList <Integer> ();
     }
     
     public void guardeElementos ()
@@ -24,11 +27,28 @@ public class Nota
         boolean hayElementos = true;
         while (hayElementos)
         {
-            //Pregunta con JOption lo que quiere ingresar y lo guarda en una string
-            String elemento = null;
-            lista.add(elemento);
+            String elemento = (String)(JOptionPane.showInputDialog(null,"Ingrese tipo de elemento."));
+            int cantidad = 0;
+            boolean invalid = true;
+            while(invalid) {
+                try {
+                    String s = (String) (JOptionPane.showInputDialog(null,"Ingrese la cantidad del elemento."));
+                    cantidad = Integer.parseInt(s);
+                    invalid = false;
+                    if (cantidad < 0) {
+                        JOptionPane.showMessageDialog(null,"¡El número debe ser un entero positivo!");
+                        invalid = true;
+                    }
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null,"¡Ingrese un número válido!");
+                }
+            }
+            recurso.add(elemento);
+            cantidades.add(cantidad);
             
-            //Pregunta con JOption si hay más elementos a ingresar en la lista. Si no es así hace hayElementos = false
+            String [] ops = {"Sí","No"};
+            String ans = (String)(JOptionPane.showInputDialog(null,"Desea ingresar otro elemento?","Por favor escoja una opción",JOptionPane.QUESTION_MESSAGE, null, ops, ops[0]));
+            hayElementos = (ans.equals("Sí"))?true:false;
         }
     }
 }
